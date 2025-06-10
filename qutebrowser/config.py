d11bfceeab4a7defe55396d7aca374c4817847a9
ezuloaga0@gmail.com <ezuloaga0@gmail.com>
2025-06-10 12:19:17 -0400

    Add qutebrowser config

---
+++ qutebrowser/config.py
@@ -0,0 +1,10 @@
+## Load autoconfig.yml
+config.load_autoconfig(True)
+c.aliases = {'w': 'session-save', 'q': 'close', 'qa': 'quit', 'wq': 'quit --save', 'wqa': 'quit --save'}
+c.auto_save.interval = 15000
+c.auto_save.session = True
+c.backend = 'webengine'
+c.changelog_after_upgrade = 'patch'
+c.url.searchengines = {'DEFAULT': 'https://www.startpage.com/do/search?query={}'}
+c.url.start_pages = ['https://www.startpage.com/']
+
