6e60ac29c23ddfdf72274401fc39df73146114d8
ezuloaga0@gmail.com <ezuloaga0@gmail.com>
2025-07-17 15:24:23 -0400

    Home directory nonsense

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -5,7 +5,7 @@ c.backend = 'webengine'
 c.changelog_after_upgrade = 'patch'
 c.url.searchengines = {'DEFAULT': 'https://search.brave.com/search?q={}&source=web'}
 c.url.start_pages = ['https://search.brave.com/']
-c.downloads.location.directory = '$HOME/downloads'
+c.downloads.location.directory = '$HOME/Downloads'
 c.colors.webpage.bg = 'black'
 c.colors.webpage.darkmode.enabled = True
 c.content.user_stylesheets = [
