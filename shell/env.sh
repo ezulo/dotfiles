21d91205350edb11096f586bd46f7b199ac9ac22
Eduardo Zuloaga <ezuloaga0@gmail.com>
2026-05-06 14:18:07 -0400

    Amend shell env

--- shell/env.sh
+++ shell/env.sh
@@ -8,17 +8,12 @@ else
     export EDITOR='nvim'
 fi
 
-if [[ -d "$XDG_RUNTIME_DIR/hypr" ]]; then
-    export HYPRLAND_LOG_FILE="$XDG_RUNTIME_DIR/hypr/$(ls -t $XDG_RUNTIME_DIR/hypr/ | head -n 1)/hyprland.log"
-fi
-export KITTY_CONFIG_DIRECTORY=$HOME/.config/kitty
-export WAYBAR_CONFIG_DIRECTORY=$HOME/.config/waybar
-export HYPRPAPER_CONFIG_DIRECTORY=$HOME/.config/hypr
-export THEME_DIR="$HOME/.config/theme"
-export THEME="$(cat $THEME_DIR/current-theme)"
-export TERM="kitty"
-
-export ZEIT_DB="$HOME/.config/zeit/zeit.db"
+# if [[ -d "$XDG_RUNTIME_DIR/hypr" ]]; then
+#     export HYPRLAND_LOG_FILE="$XDG_RUNTIME_DIR/hypr/$(ls -t $XDG_RUNTIME_DIR/hypr/ | head -n 1)/hyprland.log"
+# fi
+# export WAYBAR_CONFIG_DIRECTORY=$HOME/.config/waybar
+# export HYPRPAPER_CONFIG_DIRECTORY=$HOME/.config/hypr
+export TERM="alacritty"
 
 # esp-idf
 export ESP32_COMPONENT_HOME="$HOME/Projects/esp32/esp32_components"
